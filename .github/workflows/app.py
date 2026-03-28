dacbbb25b82bad3227a15072b17ab4bfac708585
Divya-457 <divya.2301050@srec.ac.in>
2026-03-28 14:29:57 +0530

    Update app.py

--- .github/workflows/app.py
+++ .github/workflows/app.py
@@ -5,6 +5,6 @@ def multiply(a, b):
     return a * b
 
 if __name__ == "__main__":
-    print("CI/CD Pipeline Running 🚀")
+    print("CI/CD Pipeline Running...")
     print("Addition:", add(5, 3))
     print("Multiplication:", multiply(5, 3))
