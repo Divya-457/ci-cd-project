39bb91e7f7ff9d169cce46ed716db795653be906
Divya-457 <divya.2301050@srec.ac.in>
2026-03-28 14:28:53 +0530

    Create app.py

---
+++ .github/workflows/app.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    return a + b
+
+def multiply(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    print("CI/CD Pipeline Running 🚀")
+    print("Addition:", add(5, 3))
+    print("Multiplication:", multiply(5, 3))
